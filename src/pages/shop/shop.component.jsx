79721d7ebacf804da6020e6ba443604dea18f5e9
adelincarabas <adelinromana@gmail.com>
2021-10-09 11:41:30 +0300

    finished the header, the big erorr was that we put curly brackets instead of normal brackets :)

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,8 @@
-import { render } from "@testing-library/react";
 import React from "react";
 
-import SHOP_DATA from "./shop.data.jsx";
+import SHOP_DATA from "./shop.data.js";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
